5e289c368d5b0befb7e7275aa1e7e62676ae963e
jFarrow02 <jack.dempsey.farrow@gmail.com>
2021-11-20 23:25:00 -0500

    code cleanup

--- scripts/sql/createRestaurantsSql.js
+++ scripts/sql/createRestaurantsSql.js
@@ -8,22 +8,7 @@ const stripChars = (name, separator, locatorChars, joiner) => {
     const nameAsArray = name.split(separator);
     let transformedArray = [...nameAsArray];
     let transformedName;
-    // if(name === "Chic-Fil-A, Quiznos Subs, Jw'S, Tossed") {
-    //     console.log(nameAsArray); // [ 'Chic-Fil-A,', 'Quiznos', 'Subs,', "Jw'S,", 'Tossed' ]
-    // }
     nameAsArray.forEach((value, valueIndex) => {
-        // if(name === "Chic-Fil-A, Quiznos Subs, Jw'S, Tossed") {
-        //     console.log(valueIndex + ":" + nameAsArray[valueIndex]);
-        //     // 0:Chic-Fil-A,
-        //     // 1:Quiznos
-        //     // 2:Subs,
-        //     // 3:Jw'S,
-        //     // 4:Tossed
-        // }
-        
-        // if(name === "Chic-Fil-A, Quiznos Subs, Jw'S, Tossed") {
-        //     console.log(transformedArray);
-        // }
         locatorChars.forEach((char) => {
             if(value.indexOf(char) !== -1){
                 transformedName = value.split(char).filter((n) => n !== char).join(joiner);
@@ -31,36 +16,15 @@ const stripChars = (name, separator, locatorChars, joiner) => {
             }
         });
     });
-    // if(name === "Chic-Fil-A, Quiznos Subs, Jw'S, Tossed") {
-    //     console.log(transformedArray); // [ 'Chic-Fil-A,', 'Quiznos', 'Subs,', 'Jw\\S,', 'Tossed' ]
-    // }
     return transformedName ? [...transformedArray].join(' ') : name;
 };
 
-// const buildSqlStatementFromRestaurantRecord = (data) => {
-//     const textAsArray = data.split('\n');
-//     const maxIndex = textAsArray.length -1;
-//     let sqlStatement = '';
-//     textAsArray.forEach((restaurantRecord, idx) => {
-//         const recordArray = restaurantRecord.split(',');
-//         const [ restaurantId, borough, building, cuisine, latitude, longitude, name, street, zip ] = recordArray;
-//         const sql = `${PREFIX}'${restaurantId}, ${borough}, ${building}, ${cuisine}, ${latitude}, ${longitude}, ${name}, ${street}, ${zip}${SUFFIX}`;
-//         sqlStatement = idx === maxIndex ? sqlStatement + sql : sqlStatement + `${sql}\n`;
-//     });
-    
-//     return sqlStatement;
-// };
 const buildSqlStatementFromRestaurantRecord = (data) => {
     const textAsArray = data.split('\n');
     const maxIndex = textAsArray.length -1;
     let sqlStatement = '';
     textAsArray.forEach((statement, idx) => {
         const sql = `${PREFIX}${statement}${SUFFIX}`;
-        //sqlStatement+= sql;
-
-        // const recordArray = restaurantRecord.split(',');
-        // const [ restaurantId, borough, building, cuisine, latitude, longitude, name, street, zip ] = recordArray;
-        // const sql = `${PREFIX}'${restaurantId}, ${borough}, ${building}, ${cuisine}, ${latitude}, ${longitude}, ${name}, ${street}, ${zip}${SUFFIX}`;
         sqlStatement = idx === maxIndex ? sqlStatement + sql : sqlStatement + `${sql}\n`;
     });
     
@@ -94,10 +58,6 @@ const convertJSON = async () => {
         const transformedBuilding = stripChars(building, ' ', ['\'', '`'], '\\');
         const transformedStreet = stripChars(street, ' ', ['\'', '`'], '\\').split(' ')
             .filter(s => s.length > 0).join(' ');
-
-        if(restaurantId === '40634715') {
-            console.log(transformedName);
-        }
        
         text+= idx < maxIndex ? `'${restaurantId}', '${borough}', '${transformedBuilding}', '${cuisine.split(',').join('')}', ${latitude}, ${longitude}, '${transformedName}', '${transformedStreet}', '${zipcode}'\n` :
         `'${restaurantId}', '${borough}', '${transformedBuilding}', '${cuisine.split(',').join(' ')}', ${latitude}, ${longitude}, '${transformedName}', '${transformedStreet}', '${zipcode}'`;
